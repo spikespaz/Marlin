c91b617ad883625b32140a46e468850e45cffc5d
Jacob Birkett <spikespaz@outlook.com>
2021-07-28 09:01:10 -0700

    Set esteps to 142

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -763,7 +763,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 144.90 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 142 }
 
 /**
  * Default Max Feed Rate (mm/s)
